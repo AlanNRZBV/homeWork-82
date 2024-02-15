3a462bd281a2474703b015b532dfe81a916d6cef
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-02-15 14:17:36 +0600

    feat: added TrackHistory model

---
+++ models/TrackHistory.ts
@@ -0,0 +1,40 @@
+import mongoose, { model, Types } from 'mongoose';
+import User from './User';
+import Track from './Track';
+
+const Schema = mongoose.Schema;
+const TrackHistorySchema = new Schema({
+  userId:{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const user = await User.findById(value);
+        return Boolean(user);
+      },
+      message: 'User does not exist!',
+    },
+  },
+  trackId:{
+    type: Schema.Types.ObjectId,
+    ref:'Track',
+    required: true,
+    validate:{
+      validator: async (value: Types.ObjectId)=>{
+        const track = await Track.findById(value)
+        return Boolean(track)
+      },
+      message: 'Track does not exit'
+    }
+  },
+  datetime:{
+    type: Date,
+    required: true,
+    default: ()=> new Date
+  }
+})
+
+const TrackHistory = model ('TrackHistory', TrackHistorySchema);
+
+export default TrackHistory
--- routers/users.ts
+++ routers/users.ts
@@ -6,10 +6,12 @@ const usersRouter = Router();
 
 usersRouter.post('/', async(req, res, next)=>{
   try{
+
     const user = new User({
       username: req.body.username,
       password: req.body.password
-    })
+    });
+
     user.generateToken();
     await user.save()
 
@@ -19,6 +21,31 @@ usersRouter.post('/', async(req, res, next)=>{
     if (e instanceof mongoose.Error.ValidationError){
       return res.status(422).send(e)
     }
+  }
+})
+
+usersRouter.post('/sessions', async (req,res, next)=>{
+  try{
+    const user = await User.findOne({username: req.body.username})
+
+    if(!user){
+      return res.status(422).send({error:'Username not found!'})
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if(!isMatch){
+      return res.status(422).send({error:'Password is wrong'})
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send({message:'Username and password is correct!', user})
+
+
+  }catch (e){
+    next(e)
   }
 })
 
