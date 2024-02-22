032038ac871d1ae25d6841a539eea20d17e80cb4
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-02-22 15:29:49 +0600

    frontend: initial commit

---
+++ frontend/src/components/UI/Navigation/Navigation.tsx
@@ -0,0 +1,21 @@
+import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import {NavLink} from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography to="/" component={NavLink} variant="h6" color="white" sx={{mr:'auto', textDecoration:'none'}}>Finance Tracker</Typography>
+        <Box>
+          <Button to="/categories" component={NavLink} color="primary" variant="contained" sx={{mr:2}}>Categories</Button>
+          <LoadingButton type="button" color="warning" variant="contained">
+            Add
+          </LoadingButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navigation;
